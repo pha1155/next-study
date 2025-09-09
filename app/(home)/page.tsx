23093eeaca06de2570f122056f5cbfdfd7d7ea8f
nani <pha3113@naver.com>
2025-09-09 17:50:10 +0900

    Refactor : API_URL을 constants로 분리

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,10 @@
 import Movie from "../../components/movie";
+import { API_URL } from "../../constants";
 import styles from "../../styles/home.module.css";
 
 export const metadata = {
   title: "Home",
 };
-
-export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
   // await new Promise((resolve) => setTimeout(resolve, 5000)); // 임의로 로딩 시간 추가
