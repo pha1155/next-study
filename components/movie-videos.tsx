3fabe9744f70b921884094d48df76581d4186127
nani <pha3113@naver.com>
2025-09-09 17:41:50 +0900

    Feat : movie videos 추가

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,14 +1,27 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-videos.module.css";
 
 async function getVideos(id: string) {
-  // console.log(`Fetching videos: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 5000)); // 임의로 로딩 시간 추가
-  throw new Error("something broke..."); // 임의로 에러 발생
-  // const response = await fetch(`${API_URL}/${id}/videos`);
-  // return response.json();
+  // await new Promise((resolve) => setTimeout(resolve, 5000)); // 임의로 로딩 시간 추가
+  // throw new Error("something broke..."); // 임의로 에러 발생
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
